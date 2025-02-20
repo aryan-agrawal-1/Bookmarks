4e7940e6435843b444cff806a75f533f796bd352
“Firstname <2877689a@student.gla.ac.uk>
2025-02-20 13:48:52 +0000

    login shadcn added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
 
 // queryClient manages caching, invalidation and background updates of our queries
 const queryClient = new QueryClient();
@@ -8,7 +10,11 @@ function App() {
   return (
     // makes the queryClient available to all components as the client prop is set to queryClient
     <QueryClientProvider client={queryClient}>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path= '/login' element = {<LoginPage />}/>
+        </Routes>
+      </BrowserRouter>
     </QueryClientProvider>
   )
 }
